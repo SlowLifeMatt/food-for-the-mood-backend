afa4f2e9eaa5e9443098b4b1ced363c373934f0d
Matthew <subinmatt@gmail.com>
2023-02-15 20:53:41 -0800

    first commit

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for food_for_the_mood project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_for_the_mood.settings')
+
+application = get_asgi_application()
---
+++ foodmood_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FoodmoodAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'foodmood_app'
